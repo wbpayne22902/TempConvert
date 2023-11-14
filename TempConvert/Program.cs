3b3ebce876164292fb61845a3b4298beef411e3e
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2023-11-14 11:18:38 -0500

    Convert to ReadLine
    converted from readkey to readline

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -1,26 +1,31 @@
 ﻿//See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 double tempFrom, tempTo;
-reader:  Console.Write("C for celsius or F for Fahrenheit> ");
-ConsoleKeyInfo consoleKey = Console.ReadKey();
+string? theKey;
+reader: Console.Write("C for celsius or F for Fahrenheit> ");
+//ConsoleKeyInfo consoleKey = Console.ReadKey();
+theKey = Console.ReadLine();
 Console.WriteLine();
 Console.Write("Enter temp to convert> ");
 try
 {
     tempFrom = Convert.ToDouble(Console.ReadLine());
-    switch (Char.ToUpper(consoleKey.KeyChar))
+    if (theKey is not null)
     {
-        case 'F':
-            tempTo = (tempFrom - 32.0) * (5.0 / 9.0);
-            Console.WriteLine($"{tempFrom} degF = {tempTo} degC");
-            break;
-        case 'C':
-            tempTo = (9.0 / 5.0) * tempFrom + 32.0;
-            Console.WriteLine($"{tempFrom} degC = {tempTo} degF");
-            break;
-        default:
-            Console.WriteLine("You must enter C or F!");
-            goto reader;
+        switch (theKey.ToUpper())
+        {
+            case "F":
+                tempTo = (tempFrom - 32.0) * (5.0 / 9.0);
+                Console.WriteLine($"{tempFrom} degF = {tempTo} degC");
+                break;
+            case "C":
+                tempTo = (9.0 / 5.0) * tempFrom + 32.0;
+                Console.WriteLine($"{tempFrom} degC = {tempTo} degF");
+                break;
+            default:
+                Console.WriteLine("You must enter C or F!");
+                goto reader;
+        }
     }
 }
 catch (Exception ex)
