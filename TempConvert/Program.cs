0ce7472489f81fe71afb15c30259854313ea2b29
Wilhelm Payne <wvonhimmel@outlook.com>
2023-12-11 12:05:45 -0500

    Refactor temp conversion code
    Created functions to convert temperatures.

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -1,5 +1,7 @@
 ﻿//See https://aka.ms/new-console-template for more information
-double tempFrom, tempTo;
+using System.Diagnostics;
+
+double tempFrom;
 string? theKey;
 reader: Console.Write("C for celsius or F for Fahrenheit> ");
 //ConsoleKeyInfo consoleKey = Console.ReadKey();
@@ -13,12 +15,10 @@ try
         switch (theKey.ToUpper())
         {
             case "F":
-                tempTo = (tempFrom - 32.0) * (5.0 / 9.0);
-                Console.WriteLine($"{tempFrom} degF = {tempTo} degC");
+                Console.WriteLine($"{tempFrom} degF = {doFtoC(tempFrom)} degC");
                 break;
             case "C":
-                tempTo = (9.0 / 5.0) * tempFrom + 32.0;
-                Console.WriteLine($"{tempFrom} degC = {tempTo} degF");
+                Console.WriteLine($"{tempFrom} degC = {doCtoF(tempFrom)} degF");
                 break;
             default:
                 Console.WriteLine("You must enter C or F!");
@@ -31,4 +31,19 @@ catch (Exception ex)
     Console.WriteLine($"Error: {ex.Message}");
     Console.WriteLine("Exiting now...");
     Environment.Exit(1);
+}
+
+static double doFtoC(double degF)
+{
+    double tempTo;
+    tempTo = (degF - 32.0) * (5.0 / 9.0);
+    return tempTo;
+
+}
+
+static double doCtoF(double degC)
+{
+    double tempTo;
+                tempTo = (9.0 / 5.0) * degC + 32.0;
+    return tempTo;
 }
